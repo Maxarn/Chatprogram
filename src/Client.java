c42e947b12e2a1526439817b1dfbece0b734cbf2
Axel <pennsater@gmail.com>
2017-02-16 16:59:48 +0100

    backup

--- src/Client.java
+++ src/Client.java
@@ -1,98 +1,70 @@
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+import java.io.*;
+import java.net.*;
 
 
+public class Client extends Thread {
+    private static String USERNAME = "fuckface";
 
-public class Client extends Application {
-    private static String USERNAME = "";
-    private static boolean LOGGED_IN = false;
+    private static String SERVER = "192.168.0.100";
+    private static int PORT = 1337;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 
-    /**
-     * Post login screen with all the chat stuffz
-     */
-    private void setSceneChat(Stage primaryStage){
-        BorderPane bp = new BorderPane();
+    public static void sendMessage(String message) throws IOException {
+        InputStream is = new ByteArrayInputStream( message.getBytes( ) );
 
-        TextArea chatRenderer = new TextArea();
-        chatRenderer.setEditable(false);
-        bp.setCenter(chatRenderer);
+        Socket client = new Socket(SERVER, PORT);
 
-        HBox hBox = new HBox();
-        TextArea chatInput = new TextArea();
-        Button button = new Button("Send");
-        button.setOnAction(event -> {
-            System.out.println(String.format("%s: %s",USERNAME,chatInput.getText()));
-            chatInput.clear();
-        });
-        hBox.getChildren().addAll(chatInput,button);
-        bp.setBottom(hBox);
+        OutputStream outToServer = client.getOutputStream();
 
-        Scene scene = new Scene(bp,1024,600);
-        primaryStage.setScene(scene);
+        DataOutputStream out = new DataOutputStream(outToServer);
+        out.writeUTF(String.format("%s: %s",USERNAME, is));
+
+        InputStream inFromServer = client.getInputStream();
+        DataInputStream in = new DataInputStream(inFromServer);
+
+
+        client.close();
 
     }
 
-
-    @Override
-    public void start(Stage primaryStage) {
-        /**
-         * Login Screen
-         */
-        BorderPane bp = new BorderPane();
-        VBox loginBox = new VBox();
-        HBox hBox = new HBox();
-
-        Label ipLabel = new Label("IP");
-        TextField ipField = new TextField();
-        Label portLabel = new Label("Port");
-        TextField portField = new TextField();
-        hBox.getChildren().addAll(ipLabel,ipField,portLabel,portField);
-        loginBox.getChildren().add(hBox);
-
-        hBox = new HBox();
-        Label usernameLabel = new Label("Username");
-        TextField usernameField = new TextField();
-
-        hBox.getChildren().addAll(usernameLabel,usernameField);
-        loginBox.getChildren().add(hBox);
-
-        Button loginButton = new Button("Login");
-        loginButton.setOnAction(event -> {
-                if (validateUserInput(usernameField.getText())){
-                    setSceneChat(primaryStage);
-                }
+    public void run() {
+        String message = "";
+        try {
 
 
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            Socket client = new Socket(SERVER, PORT);
+            while(!message.toLowerCase().equals("quit")) {
+
+                OutputStream outToServer = client.getOutputStream();
+                System.out.print("\nMessage to server: ");
+                message = br.readLine();
+
+                DataOutputStream out = new DataOutputStream(outToServer);
+                out.writeUTF(String.format("%s: %s",USERNAME, message));
+
+                InputStream inFromServer = client.getInputStream();
+                DataInputStream in = new DataInputStream(inFromServer);
+
+                System.out.println(in.readUTF());
 
             }
-        );
-        loginBox.getChildren().add(loginButton);
-        bp.setCenter(loginBox);
+            System.out.println("Closing socket. GGWP.");
+            client.close();
 
-        Scene scene = new Scene(bp, 1024, 600);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    private boolean validateUserInput(String s){
-        System.out.println(String.format("Username: %s",s));
-        if (!s.isEmpty()){
-            USERNAME = s;
-            return true;
+        }catch(IOException e) {
+            e.printStackTrace();
         }
-        else { return false;}
     }
+
+
+    public static void main(String[] args) {
+
+
+        Thread t = new Client();
+        t.start();
+
+
+    }
+
 }
--- src/Server.java
+++ src/Server.java
@@ -1,27 +1,87 @@
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
 
-public class Server {
-    public static int PORT_NUMBER = 1337;
-   public static void main(String[] args) {
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Server extends Thread{
+    private static final Logger LOGGER = Logger.getLogger(Server.class.getName());
+    private static int PORT = 1337;
+    private ServerSocket serverSocket;
+    private static String chatHistory = "";
+    private static Thread t;
+    public static boolean RUNSERVER = true;
+
+
+    public Server(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        serverSocket.setSoTimeout(30000);
+    }
+
+    public void run() {
+        LOGGER.log(Level.INFO, String.format("Waiting for client on port %s...", serverSocket.getLocalPort()));
+        Socket server = null;
         try {
-            System.out.println(String.format("Server address: %s, Port: %s", InetAddress.getLocalHost().getHostAddress(),PORT_NUMBER));
-        } catch (UnknownHostException e) {
-            e.getMessage();
+
+            server = serverSocket.accept();
+            LOGGER.log(Level.INFO, String.format("Just connected to %s", server.getRemoteSocketAddress()));
+            while (true) {
+                try {
+                    DataInputStream in = new DataInputStream(server.getInputStream());
+                    DataOutputStream out = new DataOutputStream(server.getOutputStream());
+
+                    String clientMessage = in.readUTF();
+                    if (clientMessage != null){
+                        LOGGER.log(Level.INFO, String.format("%s\n", clientMessage));
+                        out.writeUTF(clientMessage);
+                    }
+
+//                out.writeUTF(String.format()"Thank you for connecting to " + server.getLocalSocketAddress()
+//                        + "\nGoodbye!");
+
+                    t.sleep(1000);
+                } catch (SocketTimeoutException s) {
+                    System.out.println("Socket timed out!");
+                    break;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    break;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    break;
+                }
+
+            }
+            server.close();
+            LOGGER.log(Level.INFO, "Stopping Server...");
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-//        try{
-//            ServerSocket ss = new ServerSocket(PORT_NUMBER);
-//            Socket s = ss.accept();
-//
-//
-//            s.close();
-//        }catch (Exception e) {
-//            // Doot
-//        }
+    }
 
+    public static void main(String[] args) {
+        LOGGER.setLevel(Level.ALL);
+        try {
+            t = new Server(PORT);
+            t.start();
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void startServer() {
+        LOGGER.setLevel(Level.INFO);
+
+        try {
+            t = new Server(PORT);
+            t.start();
+            System.out.println("Started thread");
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
