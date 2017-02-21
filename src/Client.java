72c8a51020ede6237b3c52d74da3dc802e978f78
Axel <pennsater@gmail.com>
2017-02-21 11:20:30 +0100

    multipla klienter, wow

--- src/Client.java
+++ src/Client.java
@@ -7,33 +7,50 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
-//    private static String HOST = "192.168.0.100";
+    //    private static String HOST = "192.168.0.100";
     public static void main(String[] args) {
-        if (args.length > 0) {
-            String message = (args.length > 1) ? args[1] : "LOOOOOOOL";
+        if (args.length > 1) {
+            String USERNAME = args[1];
             String HOST = args[0];
             try (Socket socket = new Socket(HOST, 1337)) {
                 System.out.println("Connected to "+HOST+":1337");
                 PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
                 BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedReader userInuput = new BufferedReader(new InputStreamReader(System.in));
 
-                System.out.println("Writing message");
-                writer.write(message + "\r\n");
-                writer.flush();
+                ClientListener cl = new ClientListener(socket);
+                Thread t = new Thread(cl);
+                t.start();
 
-                System.out.println("Reading message");
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
+
+                String s;
+                System.out.print("Write message: ");
+                while((s = userInuput.readLine()) != null){
+
+
+
+                    writer.write(USERNAME + ": " + s + "\r\n");
+                    writer.flush();
+                    /*
+                    System.out.println("Reading message");
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                    */
+
+
                 }
                 writer.close();
-                reader.close();
+//                reader.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         else{
+            System.out.println("java Client <ip> <username>");
             // TODO kukat gui kanske !? !? ?!
         }
     }
+
 }
